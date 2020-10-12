9e55f153b338db6c9ff46b4485454e7dd3e6b055
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-12 17:52:01 -0600

    Add sunk test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -12,8 +12,21 @@ class ShipTest < Minitest::Test
 
   def test_ship_health
     cruiser = Ship.new("Cruiser", 3)
-    
+
     assert_equal 3, cruiser.health
+  end
+
+  def test_ship_starts_not_sunk
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal 2, cruiser.health
+    cruiser.hit
+    assert_equal 1, cruiser.health
+    assert_equal false, cruiser.sunk?
+    cruiser.hit
+    assert_equal true, cruiser.sunk?
   end
 
 end
