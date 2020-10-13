666d0e8a94b6790f3d56bf42940496d839dcdba3
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-12 18:03:45 -0600

    Add health and sunk method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,13 +1,29 @@
 class Ship
 
-  attr_reader :name, :length
+  attr_reader :name, :health, :sunk, :length
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = @length
   end
 
-  def health
-    @length 
+  # def health
+  #   @length
+  # end
+
+  def sunk?
+    # if @health == 0
+    #   true
+    # else
+    #   @sunk
+    # end
+    @health <= 0
+
+  end
+
+  def hit
+
+    @health -= 1
   end
 end
