f0b6d1a8094c035ffe4662348bd69d68c26ba3ff
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-12 18:13:19 -0600

    Add setup method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -4,29 +4,27 @@ require './lib/ship'
 
 class ShipTest < Minitest::Test
 
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
   def test_it_exists_and_has_attributes
-    cruiser = Ship.new("Cruiser", 3)
-    assert_equal "Cruiser", cruiser.name
-    assert_equal 3, cruiser.length
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
   end
 
   def test_ship_health
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal 3, cruiser.health
+    assert_equal 3, @cruiser.health
   end
 
   def test_ship_starts_not_sunk
-    cruiser = Ship.new("Cruiser", 3)
-
-    assert_equal false, cruiser.sunk?
-    cruiser.hit
-    assert_equal 2, cruiser.health
-    cruiser.hit
-    assert_equal 1, cruiser.health
-    assert_equal false, cruiser.sunk?
-    cruiser.hit
-    assert_equal true, cruiser.sunk?
+    assert_equal false, @cruiser.sunk?
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+    @cruiser.hit
+    assert_equal 1, @cruiser.health
+    assert_equal false, @cruiser.sunk?
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
   end
 
 end
