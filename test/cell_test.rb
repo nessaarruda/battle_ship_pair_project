a5e31a7e5413c4a91fa7cc50b45d6b8b69c96891
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-12 19:12:35 -0600

    Add render test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -8,6 +8,8 @@ class CellTest < Minitest::Test
   def setup
     @cell = Cell.new("B4")
     @cruiser = Ship.new("Cruiser", 3)
+    @cell_1 = Cell.new("B4")
+    @cell_2 = Cell.new("C3")
   end
 
   def test_it_exists_and_has_attributes
@@ -28,5 +30,22 @@ class CellTest < Minitest::Test
     @cell.fire_upon
     assert_equal 2, @cell.ship.health
     assert_equal true, @cell.fired_upon?
+  end
+
+  def test_render_returns_correct_string
+    assert_equal ".", @cell_1.render
+    @cell_1.fire_upon
+    assert_equal "M", @cell_1.render
+    @cell_2.place_ship(cruiser)
+    assert_equal ".", @cell_2.render
+# Indicate that we want to show a ship with the optional argument
+    assert_equal "S", @cell_2.render(true)
+    @cell_2.fire_upon
+    assert_eual "H", @cell_2.render
+    assert_equal false, @cruiser.sunk?
+    @cruiser.hit
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
+    assert_equal "X", @cell_2.render
   end
 end
