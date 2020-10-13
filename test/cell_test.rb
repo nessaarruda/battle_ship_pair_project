449aa90f8732321846b1a12de525750f96e91647
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-12 18:28:02 -0600

    Add setup and it exists and has attributes test

---
+++ test/cell_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+require './lib/ship'
+
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_equal "B4", @cell.coordinate
+    asser_equal nil, @cell.ship
+  end
+end
