a53970ff9fe1d073ae81c57d9c8c827c6f57eb68
Cydnee Owens <cks410@yahoo.com>
2020-10-13 17:44:55 -0600

    Add cells method and test

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,15 @@
 class Board
-  attr_reader :argument
 
   def initialize
-    @argument = argument
+
   end
 
+  def cells
+    cells_hash = {}
+    coordinates = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    coordinates.each do |coordinate|
+      cells_hash[coordinate] = Cell.new(coordinate)
+    end
+    cells_hash
+  end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -12,4 +12,10 @@ class BoardTest < Minitest::Test
   def test_it_exists_and_has_attributes
     assert_instance_of Board, @board
   end
+
+  def test_cells_are_being_created
+    expected = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    assert_equal expected, @board.cells.keys
+    assert_equal 16, @board.cells.count
+  end
 end
