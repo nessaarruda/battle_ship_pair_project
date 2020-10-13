3b4714639605239ef52d319a3087b7c9d199b054
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-12 18:51:06 -0600

    Add fired upon test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -15,10 +15,18 @@ class CellTest < Minitest::Test
   end
 
   def test_ships_can_be_placed
-    assert_equal nil, @cell.ship
+    assert_nil @cell.ship
     assert_equal true, @cell.empty?
     @cell.place_ship(@cruiser)
     assert_equal @cruiser, @cell.ship
     assert_equal false, @cell.empty?
+  end
+
+  def test_its_been_fired_upon
+    @cell.place_ship(@cruiser)
+    assert_equal false, @cell.fired_upon?
+    @cell.fire_upon
+    assert_equal 2, @cell.ship.health
+    assert_equal true, @cell.fired_upon?
   end
 end
