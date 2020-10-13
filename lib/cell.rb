448cf89c8894831226b85e67b0a87221581312da
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-13 17:03:16 -0600

    Add saved changes done by accident

---
+++ lib/cell.rb
@@ -0,0 +1,42 @@
+class Cell
+  attr_reader :coordinate, :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = ship
+    @fired_upon = false
+  end
+
+  def empty?
+    @ship.nil?
+  end
+
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    if empty? == false
+      @ship.hit
+    end
+    @fired_upon = true
+  end
+
+  def render(reveal_ship = false)
+    if fired_upon? && empty?
+      "M"
+    elsif fired_upon? && empty? == false && ship.sunk?
+      "X"
+    elsif fired_upon? && @ship.sunk? == false && empty? == false
+      "H"
+    elsif reveal_ship == true && empty? == false
+      "S"
+    else
+      "."
+    end
+  end
+end
