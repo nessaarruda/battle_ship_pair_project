89d6d826b1af498f9c68d51dbd01e0276087a755
Cydnee Owens <cks410@yahoo.com>
2020-10-13 20:54:42 -0600

    Add valid placement consecutive letters and numbers method

--- lib/board.rb
+++ lib/board.rb
@@ -19,6 +19,20 @@ class Board
   end
 
   def valid_placement?(ship, coordinate)
-    coordinate.count == ship.length
+    letters = []
+    numbers = []
+    coordinate.each do |letter|
+      letters << letter[0]
+      numbers << letter[-1]
+    end
+    numbers_range = numbers[0]..numbers[-1]
+    letters_range = letters[0]..letters[-1]
+    if letters_range.to_a.uniq.length == 1 && numbers == numbers_range.to_a && coordinate.count == ship.length
+      true
+    elsif letters_range.to_a.uniq.length != 1 && letters == letters_range.to_a && numbers.all? == true && coordinate.count == ship.length
+      true
+    else
+      false
+    end
   end
 end
