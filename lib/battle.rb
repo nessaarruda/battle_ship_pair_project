4f02bb9a748a5feee37d7214b840bbce14bdc168
Cydnee Owens <cks410@yahoo.com>
2020-10-15 15:39:31 -0600

    initialize user and computer ships and board

--- lib/battle.rb
+++ lib/battle.rb
@@ -1,5 +1,21 @@
+require "./lib/board"
+require "./lib/ship"
+require "./lib/cell"
+
 class Battle
+  attr_reader :user_board,
+              :user_cruiser,
+              :user_submarine,
+              :computer_board,
+              :computer_cruiser,
+              :computer_submarine
   def initialize
+    @user_board         = Board.new
+    @user_cruiser       = Ship.new("Cruiser", 3)
+    @user_submarine     = Ship.new("Submarine", 2)
+    @computer_board     = Board.new
+    @computer_cruiser   = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
   end
 
 end
