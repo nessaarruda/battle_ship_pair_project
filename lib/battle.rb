385fef5011e746e4a706e60db6862532e83d6d40
Cydnee Owens <cks410@yahoo.com>
2020-10-15 15:57:23 -0600

    Add go to battle outline

--- lib/battle.rb
+++ lib/battle.rb
@@ -12,11 +12,11 @@ class Battle
 
   def initialize
     @user_board         = Board.new
-    @user_cruiser       = Ship.new("Cruiser", 3)
-    @user_submarine     = Ship.new("Submarine", 2)
+    @user_cruiser       = Ship.new('Cruiser', 3)
+    @user_submarine     = Ship.new('Submarine', 2)
     @computer_board     = Board.new
-    @computer_cruiser   = Ship.new("Cruiser", 3)
-    @computer_submarine = Ship.new("Submarine", 2)
+    @computer_cruiser   = Ship.new('Cruiser', 3)
+    @computer_submarine = Ship.new('Submarine', 2)
   end
 
   def welcome_message
@@ -26,6 +26,15 @@ class Battle
 
   def go_to_battle
     puts welcome_message
+    user_response = gets.chomp.downcase
+    if user_response == 'p'
+      #do something
+    elsif user_response == 'q'
+      exit
+    else
+    puts "That is not a valid choice, please input 'p' or 'q'"
+    self.go_to_battle
+    end
   end
 
 
